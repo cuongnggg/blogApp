5158c2dbc3f25f139032c92902cb7a92898db5e0
cuong <cuongnguyenkid@gmail.com>
2023-04-22 15:29:39 +0700

    [cuong][Apr2223] fixing typo error on auth.js.

--- api/controller/auth.js
+++ api/controller/auth.js
@@ -4,13 +4,14 @@ import bcrypt from 'bcryptjs'
 export const register = (req, res) => {
     //check user,password is same ?
     const q = 'SELECT * FROM users WHERE email = ? OR username = ?'
-    db.query(q, [req.body.email, req.body.username], (err, data) => {
+    db.query(q, [req.body.email, req.body.username, req.body.password], (err, data) => {
         if (err) return res.json(err)
         if (data.length) return res.status(409).json("user already exists")
-
         //Hash the password and create a user
         const salt = bcrypt.genSaltSync(10)
-        const hash = bcrypt.hashSync(res.body.password, salt)
+
+        //Typo error on hash.Misspelled on res to req
+        const hash = bcrypt.hashSync(req.body.password, salt) 
 
         const q = 'INSERT INTO users(`username`,`email`,`password`) VALUES (?)'
         const values = [
